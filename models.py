7f061df0d179c18c47f8cadd0e5b8e62a6813735
Muhammad Salman Shahid <muhammad-salman.shahid@grenoble-inp.fr>
2026-02-27 13:46:33 +0100

    pydantic classes added in models

---
+++ models.py
@@ -0,0 +1,90 @@
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel
+
+
+class LoginRequest(BaseModel):
+    """Login request model
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    username: str
+    password: str
+
+
+class QueryRequest(BaseModel):
+    """Query request mode
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    question: str
+    context_length: Optional[int] = 2
+
+
+class QueryResponse(BaseModel):
+    """Query response model
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    question: str
+    answer: str
+    cached: bool
+    response_time: float
+    user: str
+
+
+class HealthResponse(BaseModel):
+    """Health check response
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    status: str
+    query_engine_initialized: bool
+    cached_queries: int
+
+
+class DashboardResponse(BaseModel):
+    """Dashbaord metrics response
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    status: str
+    user: str
+    role: str
+    total_queries: int
+    total_users: int
+    cached_queries: int
+    avg_response_time: float
+
+
+class TokenResponse(BaseModel):
+    """Token response model
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    access_token: str
+    token_type: str
+    role: str
+
+
+class HistoryResponse(BaseModel):
+    """Query history response
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    user: str
+    query_count: int
+    history: List[Dict]
